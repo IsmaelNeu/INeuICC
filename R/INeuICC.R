56e90f66352cd296f5e0f107ee0b55a7353019bb
Ismael Neu <ismaelmmneu@hotmail.com>
2020-05-12 14:03:47 -0300

    add to GItHub

---
+++ R/INeuICC.R
@@ -0,0 +1,26 @@
+###############################################################################
+#####                                                                    ######
+#####                           Script                                   ######
+#####                       DESCRICAO PACKAGE                            ######
+#####++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++######
+#####  Author: Ismael Neu                                                ######
+#####  Date: 11/05/2020                                                  ######
+#####  Version: 1.0.1                                                    ######
+#####  E-mail: ismaelmmneu@hotmail.com                                   ######
+###############################################################################
+#'
+#' @title Coeficiente de Correla\enc{çã}{ca}o Intra-Classe
+#'
+#' @name INeuICC
+#'
+#' @description O pacote `INeuICC` re\enc{ú}{u}ne fun\enc{çõ}{co}es para a estimativa do
+#'     \emph{Coeficiente de Correla\enc{çã}{ca}o Intra-Classe}, baseado no Cap\enc{í}{i}tulo 8
+#'     do livro \strong{Modelos Biom\enc{é}{e}tricos Aplicados ao Melhoramento Gen\enc{é}{e}tico},
+#'     de Cosme Dami\enc{ã}{a}o Cruz e Adair Jos\enc{é}{e} Regazzi (1997).
+#'
+#' @references Cruz, Cosme Dami\enc{ã}{a}o & Regazzi, Adair Jos\enc{é}{e} (1997). Modelos biom\enc{é}{e}tricos
+#'    aplicados ao melhoramento gen\enc{é}{e}tico, Vi\enc{ç}{c}osa - UFV, 390p. (Tabela 8.6, p. 376)
+#'
+#' @docType package
+#'
+NULL
